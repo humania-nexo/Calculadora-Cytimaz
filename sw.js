27b39b1251d751a4a827bacf4aaac5cb76c2827c
humania-nexo <humania-nexo@users.noreply.github.com>
2026-09-07 00:12:48 -0700

    Bust cache: Actualizar version a v1.0.2 para forzar recarga de scripts en navegadores

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * ====================================================================
  */
 
-const CACHE_NAME = 'cytimaz-calc-v1.0.0';
+const CACHE_NAME = 'cytimaz-calc-v1.0.2';
 
 const ASSETS_TO_CACHE = [
   './',
